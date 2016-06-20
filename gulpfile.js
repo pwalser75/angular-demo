7792a9f909f8508169f07eb97ee9c6f06dad3c6f
Peter Walser <pwalser@frostnova.ch>
2016-06-20 10:40:31 +0200

    Updated gulp build process

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,13 @@
-var gulp = require('gulp')
-var sass = require('gulp-sass')
-var connect = require('gulp-connect')
-var browserify = require('browserify')
-var source = require('vinyl-source-stream')
+const gulp = require('gulp');
+const clean = require('gulp-clean');
+const sourcemaps = require('gulp-sourcemaps');
+const source = require('vinyl-source-stream');
+const buffer = require('vinyl-buffer');
+const browserify = require('browserify');
+const watchify = require('watchify');
+const babel = require('babelify');
+const sass = require('gulp-sass')
+const connect = require('gulp-connect')
 
 gulp.task('connect', function () {
 	connect.server({
@@ -11,26 +16,42 @@ gulp.task('connect', function () {
 	})
 })
 
-gulp.task('browserify', function() {
-    return browserify('src/web/app.js')
-        .bundle()
-        .pipe(source('main.js'))
-        .pipe(gulp.dest('build'));
-})
+var bundler = watchify(browserify('./src/web/index.js', { debug: true }).transform(babel));
 
-gulp.task('copy-static', function() {
-    gulp.src([
-		'src/web/**/*.html', 
-		'src/web/**/*.jpg', 
-		'src/web/**/*.png'
-	]).pipe(gulp.dest('build'));
-})
+function cleanTarget() {
+	gulp.src('build', {read: false})
+		.pipe(clean());
+}
+function compile() {
+    bundler.bundle()
+      .on('error', function(err) { console.error(err); this.emit('end'); })
+      .pipe(source('build.js'))
+      .pipe(buffer())
+      .pipe(sourcemaps.init({ loadMaps: true }))
+      .pipe(sourcemaps.write('./'))
+      .pipe(gulp.dest('./build'));
+}
 
-gulp.task('watch', function() {
-	gulp.watch('src/web/**/*.js', ['browserify']);
-	gulp.watch(['src/web/*.html','src/web/images/**/*.*'], ['copy-static']);
-})
+function copyResources() {
+	gulp.src([
+			'src/web/**/*.html', 
+			'src/web/**/*.jpg', 
+			'src/web/**/*.png'
+		]).pipe(gulp.dest('build'));
+}
 
-gulp.task('build', ['copy-static','browserify'])
+function watch() {
+	bundler.on('update', function() {
+		console.log('-> bundling...');
+		compile();
+	});
+	gulp.watch(['./src/web/*.html','src/web/images/**/*.*'], ['copyResources']);
+};
 
-gulp.task('default', ['build', 'connect', 'watch'])
+gulp.task('clean', function() { return cleanTarget(); });
+gulp.task('compile', function() { return compile(); });
+gulp.task('copyResources', function() { return copyResources(); });
+gulp.task('watch', function() { return watch(); });
+
+gulp.task('build', ['clean','compile','copyResources']);
+gulp.task('default', ['build', 'watch', 'connect']);
